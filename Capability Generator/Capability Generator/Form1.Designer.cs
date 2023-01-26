97e2bbbab759532d0a0b49b4229db54a864f013e
Poczo Andor <poczogaming@gmail.com>
2023-01-26 10:22:02 +0200

    fixing the formulas

---
+++ Capability Generator/Capability Generator/Form1.Designer.cs
@@ -0,0 +1,418 @@
+﻿namespace Capability_Generator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.Max_texbox_text = new System.Windows.Forms.TextBox();
+            this.Max_textbox = new System.Windows.Forms.Label();
+            this.Min_textbox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.plus_tol_textbox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tol_minus_textbox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column7 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column8 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column9 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column10 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column11 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column12 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column13 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column14 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column15 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column16 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column17 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column18 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column19 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column20 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column21 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column22 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column23 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column24 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column25 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column26 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column27 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column28 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column29 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column30 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(22, 40);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Genereaza";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Max_texbox_text
+            // 
+            this.Max_texbox_text.Location = new System.Drawing.Point(157, 42);
+            this.Max_texbox_text.Name = "Max_texbox_text";
+            this.Max_texbox_text.Size = new System.Drawing.Size(100, 20);
+            this.Max_texbox_text.TabIndex = 1;
+            // 
+            // Max_textbox
+            // 
+            this.Max_textbox.AutoSize = true;
+            this.Max_textbox.Location = new System.Drawing.Point(180, 26);
+            this.Max_textbox.Name = "Max_textbox";
+            this.Max_textbox.Size = new System.Drawing.Size(51, 13);
+            this.Max_textbox.TabIndex = 2;
+            this.Max_textbox.Text = "Maximum";
+            // 
+            // Min_textbox
+            // 
+            this.Min_textbox.Location = new System.Drawing.Point(279, 43);
+            this.Min_textbox.Name = "Min_textbox";
+            this.Min_textbox.Size = new System.Drawing.Size(100, 20);
+            this.Min_textbox.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(302, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Minimum";
+            // 
+            // plus_tol_textbox
+            // 
+            this.plus_tol_textbox.Location = new System.Drawing.Point(482, 43);
+            this.plus_tol_textbox.Name = "plus_tol_textbox";
+            this.plus_tol_textbox.Size = new System.Drawing.Size(100, 20);
+            this.plus_tol_textbox.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(494, 26);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(75, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Toleranta Plus";
+            // 
+            // tol_minus_textbox
+            // 
+            this.tol_minus_textbox.Location = new System.Drawing.Point(620, 43);
+            this.tol_minus_textbox.Name = "tol_minus_textbox";
+            this.tol_minus_textbox.Size = new System.Drawing.Size(100, 20);
+            this.tol_minus_textbox.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(630, 26);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(83, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Toleranta Minus";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(203, 256);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(336, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Te rog sa folosest PUNCT in loc de VIRGULA cand completezi datele";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4,
+            this.Column5,
+            this.Column6,
+            this.Column7,
+            this.Column8,
+            this.Column9,
+            this.Column10,
+            this.Column11,
+            this.Column12,
+            this.Column13,
+            this.Column14,
+            this.Column15,
+            this.Column16,
+            this.Column17,
+            this.Column18,
+            this.Column19,
+            this.Column20,
+            this.Column21,
+            this.Column22,
+            this.Column23,
+            this.Column24,
+            this.Column25,
+            this.Column26,
+            this.Column27,
+            this.Column28,
+            this.Column29,
+            this.Column30});
+            this.dataGridView1.Location = new System.Drawing.Point(22, 82);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(766, 150);
+            this.dataGridView1.TabIndex = 11;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Column1";
+            this.Column1.Name = "Column1";
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Column2";
+            this.Column2.Name = "Column2";
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Column3";
+            this.Column3.Name = "Column3";
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "Column4";
+            this.Column4.Name = "Column4";
+            // 
+            // Column5
+            // 
+            this.Column5.HeaderText = "Column5";
+            this.Column5.Name = "Column5";
+            // 
+            // Column6
+            // 
+            this.Column6.HeaderText = "Column6";
+            this.Column6.Name = "Column6";
+            // 
+            // Column7
+            // 
+            this.Column7.HeaderText = "Column7";
+            this.Column7.Name = "Column7";
+            // 
+            // Column8
+            // 
+            this.Column8.HeaderText = "Column8";
+            this.Column8.Name = "Column8";
+            // 
+            // Column9
+            // 
+            this.Column9.HeaderText = "Column9";
+            this.Column9.Name = "Column9";
+            // 
+            // Column10
+            // 
+            this.Column10.HeaderText = "Column10";
+            this.Column10.Name = "Column10";
+            // 
+            // Column11
+            // 
+            this.Column11.HeaderText = "Column11";
+            this.Column11.Name = "Column11";
+            // 
+            // Column12
+            // 
+            this.Column12.HeaderText = "Column12";
+            this.Column12.Name = "Column12";
+            // 
+            // Column13
+            // 
+            this.Column13.HeaderText = "Column13";
+            this.Column13.Name = "Column13";
+            // 
+            // Column14
+            // 
+            this.Column14.HeaderText = "Column14";
+            this.Column14.Name = "Column14";
+            // 
+            // Column15
+            // 
+            this.Column15.HeaderText = "Column15";
+            this.Column15.Name = "Column15";
+            // 
+            // Column16
+            // 
+            this.Column16.HeaderText = "Column16";
+            this.Column16.Name = "Column16";
+            // 
+            // Column17
+            // 
+            this.Column17.HeaderText = "Column17";
+            this.Column17.Name = "Column17";
+            // 
+            // Column18
+            // 
+            this.Column18.HeaderText = "Column18";
+            this.Column18.Name = "Column18";
+            // 
+            // Column19
+            // 
+            this.Column19.HeaderText = "Column19";
+            this.Column19.Name = "Column19";
+            // 
+            // Column20
+            // 
+            this.Column20.HeaderText = "Column20";
+            this.Column20.Name = "Column20";
+            // 
+            // Column21
+            // 
+            this.Column21.HeaderText = "Column21";
+            this.Column21.Name = "Column21";
+            // 
+            // Column22
+            // 
+            this.Column22.HeaderText = "Column22";
+            this.Column22.Name = "Column22";
+            // 
+            // Column23
+            // 
+            this.Column23.HeaderText = "Column23";
+            this.Column23.Name = "Column23";
+            // 
+            // Column24
+            // 
+            this.Column24.HeaderText = "Column24";
+            this.Column24.Name = "Column24";
+            // 
+            // Column25
+            // 
+            this.Column25.HeaderText = "Column25";
+            this.Column25.Name = "Column25";
+            // 
+            // Column26
+            // 
+            this.Column26.HeaderText = "Column26";
+            this.Column26.Name = "Column26";
+            // 
+            // Column27
+            // 
+            this.Column27.HeaderText = "Column27";
+            this.Column27.Name = "Column27";
+            // 
+            // Column28
+            // 
+            this.Column28.HeaderText = "Column28";
+            this.Column28.Name = "Column28";
+            // 
+            // Column29
+            // 
+            this.Column29.HeaderText = "Column29";
+            this.Column29.Name = "Column29";
+            // 
+            // Column30
+            // 
+            this.Column30.HeaderText = "Column30";
+            this.Column30.Name = "Column30";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.tol_minus_textbox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.plus_tol_textbox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Min_textbox);
+            this.Controls.Add(this.Max_textbox);
+            this.Controls.Add(this.Max_texbox_text);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Capability generator";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox Max_texbox_text;
+        private System.Windows.Forms.Label Max_textbox;
+        private System.Windows.Forms.TextBox Min_textbox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox plus_tol_textbox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tol_minus_textbox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column6;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column7;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column8;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column9;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column10;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column11;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column12;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column13;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column14;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column15;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column16;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column17;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column18;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column19;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column20;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column21;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column22;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column23;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column24;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column25;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column26;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column27;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column28;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column29;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column30;
+    }
+}
+
---
+++ Capability Generator/Capability Generator/Form1.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Capability_Generator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private static Random random = new Random();
+        public double GetRandomNumber(double minimum, double maximum)
+        {
+            double values = (random.NextDouble() * (maximum - minimum) + minimum);
+            return values;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double maximum = double.Parse(Max_texbox_text.Text);
+            double minimum = double.Parse(Min_textbox.Text);
+            double maximumTOL = double.Parse(plus_tol_textbox.Text);
+            double minimumTOL = double.Parse(tol_minus_textbox.Text);
+
+            double resultCPK = 0;
+            double resultCP = 0;
+            int nrOfLoops =0;
+            while ((resultCP < 1.75) && (resultCPK < 1.5)&&(nrOfLoops!=10000))
+            {
+                int numberOfValues = 30;
+                double[] values = new double[numberOfValues];
+                for (int i = 0; i < 30; i++)
+                {
+                    double random = GetRandomNumber(maximum, minimum);
+                    values[i] = random;
+                }
+                // Sum up values and squares.
+                double USL = maximumTOL;
+                double LSL = minimumTOL;
+
+                double sumOfRandomnumbers = -1;
+                Array.ForEach(values, i => sumOfRandomnumbers += i);
+                double sumsq = Math.Pow(sumOfRandomnumbers, 2);
+
+                double delta = USL - LSL;
+                sumOfRandomnumbers += delta;
+                sumsq += delta * delta;
+
+
+                // Compute standard deviation.
+
+                double mean = sumOfRandomnumbers / 30;
+                var minusAvarage = new double[values.Length];
+                var square = new double[values.Length];
+                for (int i = 0, j = values.Length; i < j; i++)
+                { minusAvarage[i] = values[i] - mean; }
+                for (int i = 0, j = values.Length; i < j; i++)
+                { square[i] = Math.Pow(minusAvarage[i], 2); }
+                double summOfSquare = square.Sum();
+                double divideByCountOfValues = summOfSquare / 30;
+                double standardDev = Math.Sqrt(divideByCountOfValues);
+
+
+                // Compute Cp and Cpk.
+
+
+                double usl = USL;
+                double lsl = LSL;
+                double Cp = (usl - lsl) / (6 * standardDev);
+                double Cpk = Math.Min(
+                    (usl - mean) / (3 * standardDev),
+                    (mean - lsl) / (3 * standardDev));
+                resultCPK = Cpk;
+                resultCP = Cp;
+                
+                if ((resultCPK > 1.5)&& (resultCP > 1.75))
+                {
+                    MessageBox.Show("Cpk este:"+resultCPK.ToString()+ "si Cp este:" + resultCP.ToString());
+                    int printDatagridwiev = 30;
+                    int loopsDatagridwiev = 0;
+                    while (printDatagridwiev > loopsDatagridwiev)
+                    {
+                        DataTable dt = new DataTable();
+                        dataGridView1.Rows[0].Cells[loopsDatagridwiev].Value = values[loopsDatagridwiev];
+                        loopsDatagridwiev++;
+                    }
+                    
+                        
+                        
+                    Clipboard.Clear();
+                    dataGridView1.SelectAll();
+                    dataGridView1.ClipboardCopyMode = DataGridViewClipboardCopyMode.EnableWithoutHeaderText;
+                    DataObject dataObj = dataGridView1.GetClipboardContent();
+                    Clipboard.SetDataObject(dataObj, true);
+                }
+
+                //Clipboard.Clear();
+                //Clipboard.SetText(string.Join("' '", values.Select(t => t.ToString())));
+                if (nrOfLoops == 10000 - 2)
+                {
+                    MessageBox.Show("Nu am gasit te rog schimba minimum si maximum");
+                }
+                nrOfLoops++;
+            }
+
+        }
+    }
+
+}
+
+
